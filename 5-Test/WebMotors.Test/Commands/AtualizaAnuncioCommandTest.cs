330e1738101415f316da0dec8fedb07c9102074a
Marcus Fernando Correa Barboxz <marcusfcbarbosa@hotmail.com>
2022-01-08 20:21:30 -0300

    Camada de Teste criada

---
+++ 5-Test/WebMotors.Test/Commands/AtualizaAnuncioCommandTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebMotors.Domain.WebMotorsContext.Commands.Input;
+
+namespace WebMotors.Test.Commands
+{
+    [TestClass]
+    public class AtualizaAnuncioCommandTest
+    {
+        private readonly AtualizaAnuncioCommand _invalidCommand = new AtualizaAnuncioCommand {Id =0,Marca = string.Empty,Modelo = string.Empty, Versao = string.Empty, Ano =0, Quilometragem =0,Observacao= String.Empty };
+        private readonly AtualizaAnuncioCommand _validCommand = new AtualizaAnuncioCommand { Id = 1, Marca = "volkswagen", Modelo = "Amarok", Versao = "Blas", Ano = 2022, Quilometragem = 10, Observacao = String.Empty };
+
+
+        public AtualizaAnuncioCommandTest()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+        //RED GREEN REFACTOR
+        [TestMethod]
+        public void DadoCommandoInvalido()
+        {
+            Assert.AreEqual(_invalidCommand.Valid, false);
+        }
+
+        [TestMethod]
+        public void DadoCommandovalido()
+        {
+            Assert.AreEqual(_validCommand.Valid, true);
+        }
+    }
+}
---
+++ 5-Test/WebMotors.Test/Commands/CriaAnuncioCommandTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebMotors.Domain.WebMotorsContext.Commands.Input;
+
+namespace WebMotors.Test.Commands
+{
+    [TestClass]
+    public class CriaAnuncioCommandTest
+    {
+        private readonly CriaAnuncioCommand _invalidCommand = new CriaAnuncioCommand {  Marca = string.Empty, Modelo = string.Empty, Versao = string.Empty, Ano = 0, Quilometragem = 0, Observacao = String.Empty };
+        private readonly CriaAnuncioCommand _validCommand = new CriaAnuncioCommand {  Marca = "volkswagen", Modelo = "Amarok", Versao = "Blas", Ano = 2022, Quilometragem = 10, Observacao = String.Empty };
+
+        public CriaAnuncioCommandTest()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+        //RED GREEN REFACTOR
+        [TestMethod]
+        public void DadoCommandoInvalido()
+        {
+            Assert.AreEqual(_invalidCommand.Valid, false);
+        }
+
+        [TestMethod]
+        public void DadoCommandovalido()
+        {
+            Assert.AreEqual(_validCommand.Valid, true);
+        }
+    }
+}
