4c0a87117501d482317ae7faf0e5ac91010ccebf
Marcus Fernando Correa Barboxz <marcusfcbarbosa@hotmail.com>
2022-01-08 19:56:26 -0300

    removendo referencias nao utilizadas

--- 1-Api/WebMotors.Api/Controllers/AuthenticationController.cs
+++ 1-Api/WebMotors.Api/Controllers/AuthenticationController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using WebMotors.Api.Services;
 using WebMotors.Domain.WebMotorsContext.Commands.Input;
