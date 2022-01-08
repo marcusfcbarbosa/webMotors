7cbf2ff0d10fa5aba8b9ce9b189d8873fa11d050
Marcus Fernando Correa Barboxz <marcusfcbarbosa@hotmail.com>
2022-01-08 14:45:34 -0300

    Implementando repositorios

---
+++ 2-Domain/WebMotors.Domain/WebMotorsContext/Repositories/Interfaces/IAnuncioWebMotorsRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebMotors.Domain.WebMotorsContext.Entities;
+
+namespace WebMotors.Domain.WebMotorsContext.Repositories.Interfaces
+{
+    public interface IAnuncioWebMotorsRepository : IBaseRepository<AnuncioWebMotors>
+    {
+    }
+}
---
+++ 3-Infra/WebMotors.Infra/Repositories/AnuncioWebMotorsRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebMotors.Domain.WebMotorsContext.Entities;
+using WebMotors.Domain.WebMotorsContext.Repositories.Interfaces;
+using WebMotors.Infra.SqlContext;
+
+namespace WebMotors.Infra.Repositories
+{
+    public class AnuncioWebMotorsRepository : BaseRepository<AnuncioWebMotors>, IAnuncioWebMotorsRepository
+    {
+        private readonly WebMotorsContext _context;
+        public AnuncioWebMotorsRepository(WebMotorsContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+    }
+}
